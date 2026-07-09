b407cfe5b0cabc092945e0b621f3ef82fd71cf66
cristhiangamer23 <rocohanocristhian12@gmail.com>
2026-07-08 23:21:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado en puerto ${PORT}`);
+    console.log(`Servidor activo en puerto ${PORT}`);
 });
