834d1f969a40483124d333cfeda5d17a3423ccc3
cristhiangamer23 <rocohanocristhian12@gmail.com>
2026-07-09 00:17:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,44 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+
+// Página principal
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+
+// CALLBACK DE DISCORD
+app.get("/auth/discord/callback", (req, res) => {
+
+    const code = req.query.code;
+
+
+    if (!code) {
+        return res.send("No se recibió código de Discord");
+    }
+
+
+    console.log("Código recibido de Discord:", code);
+
+
+    // Después aquí se hará el intercambio
+    // del código por el token de Discord
+
+
+    res.redirect("/perfil");
+
+});
+
+
+// Perfil
+app.get("/perfil", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "index.html"));
+
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 
